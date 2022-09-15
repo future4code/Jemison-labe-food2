5bc81fe1c77b8daa23bb50cc90f558dffd0b876e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-09-15 16:45:09 -0300

    Hook useForm criado

---
+++ src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const UseForm = (initialState) => {
+    const [form, setForm] = useState(initialState);
+
+    const onChange = (event) => {
+        const {name, value} = event.target;
+
+        setForm({...form, [name]: value})
+    };
+
+    const clear = () =>{
+        setForm(initialState)
+    }
+
+
+
+    return { form, onChange, clear}
+}
