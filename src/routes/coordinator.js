f089450e9b54d4dbf6ba1bbb236474ba3ebc3b0a
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-09-14 21:14:34 -0300

    criado coordinator

---
+++ src/routes/coordinator.js
@@ -0,0 +1,29 @@
+export const goToFeedPage = (navigate) =>{
+    navigate("/")
+}
+
+export const goToLoginPage = (navigate) =>{
+    navigate("/login")
+}
+
+export const goToAddressPage = (navigate) =>{
+    navigate("/address")
+}
+
+
+export const goToCartPage = (navigate) =>{
+    navigate("/cart")
+}
+
+
+export const goToProfilePage = (navigate) =>{
+    navigate("/profile")
+}
+
+export const ggoToRestaurantDetailsPage = (navigate) =>{
+    navigate(`/restaurantdetails/${id}`)
+}
+
+export const goToEditPage = (navigate) => {
+    navigate("/edit")
+}
