e49705344ae2a6053c02f29cd9ff50bb9735bbf9
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-17 19:41:13 -0300

    Adicionei animação na StartPage

--- src/pages/StartPage/StyledStartPage.js
+++ src/pages/StartPage/StyledStartPage.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import logoBranca from "../../assets/img/logo-branca.svg";
+import logoBranca from "../../assets/img/startPage/logo-branca.svg";
+import logoBrancaFechada from "../../assets/img/startPage/logo-branca-fechada.svg";
 
 export const HomeContainer = styled.div`
     display: grid;
@@ -16,11 +17,35 @@ export const HomeContainer = styled.div`
         &::after {
             display: block;
             content: "";
-            background: url(${logoBranca}) no-repeat;
+            background: url(${logoBranca});
             background-size: contain;
             width: 7.875rem;
             height: 4.063rem;
+            animation: zoomOut 1.3s linear;
+
+            @keyframes zoomOut {
+                0% {
+                    background: url(${logoBranca});
+                    transform: scale(1.7);
+                }
+                50% {
+                    background: url(${logoBranca});
+                    transform: scale(1);
+                }
+                75% {
+                    background: url(${logoBranca});
+                    transform: scale(1);
+                }
+
+                95% {
+                    transform: scale(1.1);
+                }
+
+                100% {
+                    background: url(${logoBrancaFechada});
+                    transform: scale(1);
+                }
+            }
         }
     }
 `;
-
