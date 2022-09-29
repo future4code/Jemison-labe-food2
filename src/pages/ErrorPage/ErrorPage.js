49982da78ea7c45d3fc11fa52177501b23c992a0
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-28 22:43:06 -0300

    :lipstick: :feat Adicionei uma página de erros personalizada
    Para testar, basta colocar um final aleatório na url do site

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,11 +1,27 @@
 import React from "react";
-
-import { Container } from "../../Styled";
+import { useNavigate } from "react-router-dom";
+import { Botao } from "../../components/RegistrationPages/StyledRegistrationPages";
+import { goToFeedPage } from "../../routes/coordinator";
+import { Container2, Contents2 } from "./StyledErroPage";
 
 export function ErrorPage() {
+    const navigate = useNavigate();
     return (
-        <Container>
-            <h1>Pagina ErrorPage</h1>
-        </Container>
+        <Container2>
+            <Contents2>
+                <div>
+                    <p>Ops! :(</p>
+                    <p>Página não encontrada!</p>
+                    <p>
+                        Parece que houve um erro com a página procurada. É
+                        possível que a página tenha sido removida ou o endereço
+                        não existe.
+                    </p>
+                </div>
+                <Botao onClick={() => goToFeedPage(navigate)}>
+                    Voltar a Página Principal
+                </Botao>
+            </Contents2>
+        </Container2>
     );
 }
